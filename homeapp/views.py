c4e47e93d1fb13f74a5aed8d0b92d4c11b525370
Joshua <45085742+JT4life@users.noreply.github.com>
2020-06-15 19:08:24 -0500

    gmail email

--- homeapp/views.py
+++ homeapp/views.py
@@ -55,7 +55,7 @@ def sendMessage(request):
 def mail(request):
     subject = "Greetings"
     msg = "Congratulations for your success"
-    to = "joshua4lyfe@gmail.com"
+    to = "username@gmail.com"
     res = send_mail(subject, msg, settings.EMAIL_HOST_USER, [to])
     if (res == 1):
         msg = "Mail Sent Successfuly"
